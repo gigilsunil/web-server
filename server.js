699d597cc298a6e762f4e746fc6ba65f032f8d8d
Gigil Mathew <gigil.sunil@gmail.com>
2017-02-06 16:00:09 +1000

    Initialise repo

---
+++ server.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+
+/*app.get('/',function(req, res){
+	res.send('Hello Express');
+});*/
+
+var middleware ={
+	requireAuthentication : function(req, res, next){
+		console.log('Private route hit');
+		next();
+	},
+	logger : function(req, res, next){
+		console.log(req.method + ' '+req.originalUrl);
+		next();
+	}
+};
+app.use(middleware.logger);
+//app.use(middleware.requireAuthentication);
+
+app.get('/about',middleware.requireAuthentication,function(req, res){
+	res.send('About us');
+});
+
+app.use(express.static(__dirname+'/public'));
+console.log(__dirname);
+app.listen(3000, function()
+	{
+		console.log("server started");
+	});
